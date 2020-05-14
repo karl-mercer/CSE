1daafd501a60f2e1b2f3a0f0e01674f1c72c50e0
Daniel Schnaars <dschnaars@greenville.k12.sc.us>
2020-05-14 03:33:12 +0000

    Added a loop so the user can complete multiple orders.

--- main.py
+++ main.py
@@ -9,22 +9,27 @@ Combo menu program for CSE.
 """
 
 total_cost = 0.0 #initialize total cost as a float type variable
-order_list = ["", "", "", 0]
+user_input = "" #create an empty string
 sandwich_pos = 0
 drink_pos = 1
 fries_pos = 2
 ketchup_pos = 3
 
-total_cost, order_list[sandwich_pos] = sandwich.choose_sandwich(total_cost)
+while user_input != "no":
+	order_list = ["", "", "", 0] #important to reset the order list to blank
 
-total_cost, order_list[drink_pos] = drink.choose_drink(total_cost)
+	total_cost, order_list[sandwich_pos] = sandwich.choose_sandwich(total_cost)
 
-total_cost, order_list[fries_pos] = fries.order_fries(total_cost)
+	total_cost, order_list[drink_pos] = drink.choose_drink(total_cost)
 
-total_cost, order_list[ketchup_pos] = ketchup.packet_order(total_cost)
+	total_cost, order_list[fries_pos] = fries.order_fries(total_cost)
 
-if beverage_size != "no" and fries_size != "no":
-	total_cost -= 1.00
+	total_cost, order_list[ketchup_pos] = ketchup.packet_order(total_cost)
 
-print("\nYour order includes a {} sandwich, {} drink, {} fry, and {} ketchup packets.".format(order_list[sandwich_pos], order_list[drink_pos], order_list[fries_pos], order_list[ketchup_pos]))
+	if order_list[drink_pos] != "no" and order_list[fries_pos] != "no":
+		total_cost -= 1.00
+
+	print("\nYour order includes a {} sandwich, {} drink, {} fry, and {} ketchup packets.".format(order_list[sandwich_pos], order_list[drink_pos], order_list[fries_pos], order_list[ketchup_pos]))
+	user_input = input("Can I get anything else for you? (yes/no)\n").strip().lower()
+
 print("Your total cost is ${}. How would you like to pay?".format(total_cost))
